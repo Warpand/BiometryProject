e3cbf0b73f8e8b22baf958d8d26f48a8cb45ac50
Maciej <maciej.rut01@gmail.com>
2025-04-28 16:54:24 +0200

    add admission accuracy

--- modules/metrics.py
+++ modules/metrics.py
@@ -1,31 +1,34 @@
+from abc import ABC, abstractmethod
+
 import torch
 from torchmetrics import Metric
 
 from data import IMPOSTOR_ID
 
 
-class ImpostorAccuracy(Metric):
+class BaseAccuracy(Metric, ABC):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.add_state("correct", default=torch.tensor(0), dist_reduce_fx="sum")
         self.add_state("total", default=torch.tensor(0), dist_reduce_fx="sum")
 
+    def compute(self) -> torch.Tensor:
+        return self.correct.float() / self.total
+
+    @abstractmethod
+    def update(self, predictions: torch.Tensor, target: torch.Tensor) -> None:
+        pass
+
+
+class ImpostorAccuracy(BaseAccuracy):
     def update(self, predictions: torch.Tensor, target: torch.Tensor) -> None:
         impostor_mask = target == IMPOSTOR_ID
         predictions = predictions[impostor_mask]
         self.correct += torch.sum(predictions == IMPOSTOR_ID)
         self.total += len(predictions)
 
-    def compute(self) -> torch.Tensor:
-        return self.correct.float() / self.total
 
-
-class MemberAccuracy(Metric):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.add_state("correct", default=torch.tensor(0), dist_reduce_fx="sum")
-        self.add_state("total", default=torch.tensor(0), dist_reduce_fx="sum")
-
+class MemberAccuracy(BaseAccuracy):
     def update(self, predictions: torch.Tensor, target: torch.Tensor) -> None:
         member_mask = target != IMPOSTOR_ID
         predictions = predictions[member_mask]
@@ -33,5 +36,10 @@ class MemberAccuracy(Metric):
         self.correct += torch.sum(predictions == target)
         self.total += len(target)
 
-    def compute(self) -> torch.Tensor:
-        return self.correct.float() / self.total
+
+class AdmissionAccuracy(BaseAccuracy):
+    def update(self, predictions: torch.Tensor, target: torch.Tensor) -> None:
+        predictions = torch.where(predictions == IMPOSTOR_ID, predictions, ~IMPOSTOR_ID)
+        target = torch.where(target == IMPOSTOR_ID, target, ~IMPOSTOR_ID)
+        self.correct += torch.sum(predictions == target)
+        self.total += len(target)
